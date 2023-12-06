47611c1b0c25ce960830cd96f0c7a3bba22ce564
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-12-06 13:49:39 -0300

    Exercícios 1, 2, 3, 4 e 5

---
+++ src/main/kotlin/Ex04.kt
@@ -0,0 +1,13 @@
+fun main() {
+    print("Digite um numero: ")
+    val x = readln().toInt()
+
+    print("Digite um numero: ")
+    val y = readln().toInt()
+
+    var quociente = x / y
+    var restoDivisao = x - quociente * y
+
+    println(quociente)
+    print(restoDivisao)
+}
---
+++ src/main/kotlin/Ex05.kt
@@ -0,0 +1,14 @@
+import kotlin.math.PI
+
+fun main() {
+    print("Digite o raio do cilindro: ")
+    val raio = readln().toDouble()
+    print("Digite a altura do cilindro: ")
+    val altura = readln().toDouble()
+
+    val volume = PI *raio * raio *altura
+    println("Valor do volume é: $volume ")
+
+    val area = 2 * PI * raio * raio + 2 * PI *raio * altura
+    print("O valor da area é: $area ")
+}

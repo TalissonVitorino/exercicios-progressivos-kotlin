41298cecb43588bb672ebda1ae82622399ea689d
Talisson Vitorino <146876572+TalissonVitorino@users.noreply.github.com>
2023-12-19 09:20:14 -0300

    Exercícios 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 19

---
+++ src/main/kotlin/Ex19.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+fun main() {
+    val teclado = Scanner(System.`in`)
+    println("Digite um número")
+    val numero1 = teclado.nextInt()
+    println("Digite um número")
+    val numero2 = teclado.nextInt()
+    println("Digite um número")
+    val numero3 = teclado.nextInt()
+    println("Digite um número")
+    val numero4 = teclado.nextInt()
+    var soma = 0
+
+    if (numero1 % 2 == 0) soma += numero1
+    if (numero2 % 2 == 0) soma += numero2
+    if (numero3 % 2 == 0) soma += numero3
+    if (numero4 % 2 == 0) soma += numero4
+    println("O total da soma é: $soma")
+}
